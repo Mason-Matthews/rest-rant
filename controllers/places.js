c656a0c5229cf6e336d9a0b4268909b70577a132
Mason-Matthews <masonmatthews98@gmail.com>
2022-06-03 18:26:01 -0700

    post route

--- controllers/places.js
+++ controllers/places.js
@@ -2,26 +2,38 @@ const { append } = require('express/lib/response')
 
 const router = require('express').Router()
 
-router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: './images/header2.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: './images/header4.jpg'
-      }]
-      
-    res.render('places/index', {places})
+
+router.get('/new', (req, res) => {
+  res.render('places/new')
 })
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('POST /places')
+})
+
 
 // GET /places
 
+router.get('/', (req, res) => {
+  let places = [{
+      name: 'H-Thai-ML',
+      city: 'Seattle',
+      state: 'WA',
+      cuisines: 'Thai, Pan-Asian',
+      pic: './images/header2.jpg'
+    }, {
+      name: 'Coding Cat Cafe',
+      city: 'Phoenix',
+      state: 'AZ',
+      cuisines: 'Coffee, Bakery',
+      pic: './images/header4.jpg'
+    }]
+    
+  res.render('places/index', {places})
+
   
+  
+})
 
 module.exports = router
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -6,26 +6,27 @@ function index (data) {
         return (
           <div className="col-sm-6">
             <h2>{place.name}</h2>
-            <p clasName="text-center">
+            <p className="text-center">
               {place.cuisines}
             </p>
             <img src={place.pic} alt={place.name} width="500" height="400" />
-            <p clasName="text-center">
+            <p className="text-center">
               Located in {place.city}, {place.state}
             </p>
           </div>
         )
       })
       
-    return (
-      <Def>
-          <main>
-              <h1>Places to Rant or Rave About</h1>
-              <div className="row">
-                {placesFormatted}
-              </div>
-          </main>
-      </Def>
+      return (
+        <Def>
+            <main>
+                <h1>Places to Rant or Rave About</h1>
+                <div className="row">
+                    {placesFormatted}
+                </div>
+            </main>
+            <div className='footer'>Author: Ryan Rasmussen</div>
+        </Def>
     )
   }
   
